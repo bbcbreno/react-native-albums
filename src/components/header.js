a786c0764fecfc996cdd6c7123dd19f7847fd2fd
BBC and Huds <contato+bbc+huds@devsup.com.br>
2017-04-07 16:39:32 -0300

    style for header

--- src/components/header.js
+++ src/components/header.js
@@ -13,7 +13,18 @@ const Header = () => {
 
 const styles = {
   viewStyle: {
-    backgroundColor: '#F8F8F8'
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',  //for vertical: flex-start | flex-end
+    alignItems: 'center', //for horizontal: flex-start | flex-end
+    height: 60,
+    paddingTop: 15,
+
+    shadowColor: '#222',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+
+    elevation: 2,
+    position: 'relative'
   },
   textStyle: {
     fontSize: 20
